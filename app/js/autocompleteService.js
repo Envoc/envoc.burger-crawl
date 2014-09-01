c6956f4aa18bda8853266f4a7bfc39b893357b41
Justin Obney <justinobney@gmail.com>
2014-09-01 18:27:45 -0500

    feat(autocomplete): add Google place auto suggestions

---
+++ app/js/autocompleteService.js
@@ -0,0 +1,29 @@
+app.service('autocompleteService', function($q, $cordovaGeolocation) {
+  var self = this;
+  var service = new google.maps.places.AutocompleteService();
+  var coords = {};
+
+  $cordovaGeolocation.getCurrentPosition().then(function(position) {
+    coords = position.coords;
+  });
+
+  // see: https://developers.google.com/maps/documentation/javascript/reference#QueryAutocompletionRequest
+  self.getQueryPredictions = function(queryAutocompletionRequest) {
+    var dfd = $q.defer();
+
+    if(coords.latitude){
+      queryAutocompletionRequest.location = new google.maps.LatLng(coords.latitude, coords.longitude);
+      queryAutocompletionRequest.radius = 25
+    }
+    
+    service.getQueryPredictions(queryAutocompletionRequest, function callback(predictions, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        dfd.reject(status);
+        return;
+      }
+      dfd.resolve(predictions);
+    });
+
+    return dfd.promise;
+  }
+});
